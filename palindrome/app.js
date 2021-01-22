e29eba677b3f318b9dc093d8a4a069b127ec1c56
Alex Reid <alexatrs@hotmail.co.uk>
2021-01-22 13:07:36 +0000

    updated the app

--- palindrome/app.js
+++ palindrome/app.js
@@ -1,9 +1,13 @@
 const input = document.getElementById('input-field');
 const btn = document.getElementById('btn');
-const answerDiv = document.getElementById('answer');
+const answerSentance = document.getElementById('answerSentance');
+const span = document.getElementById('answerSpan');
+const answerDiv = document.getElementById('answer')
+
 
 const checkIfPalindrome = () => {
-    answerDiv.innerHTML = '';
+    answerSentance.innerHTML = '';
+    span.innerHTML = '';
     // Check if input is a string
     if (input.value === '' || input.value === undefined) {
         const noWord = document.createElement("H3");
@@ -21,20 +25,19 @@ const checkIfPalindrome = () => {
             backwardsWord.unshift(splitWord[i]);
         }
         let newWord = backwardsWord.join('');
-        console.log(newWord)
         //check if input var and reverse var are the same
         if (newWord === word) {
             //if they are the same update the DOM with Palindrome confirmation
-            const isPalindrome = document.createElement("H3");
-            isPalindrome.innerHTML = `${word} IS A PALINDROME!`;
-            answerDiv.appendChild(isPalindrome)
+            span.innerHTML = word;
+            console.log(span);
+            answerSentance.innerHTML = `IS A PALINDROME!`;
         } else {
             // else update the DOM with non plaindrome confirmation
-            const isNotPalindrome = document.createElement("H3");
-            isNotPalindrome.innerHTML = `${word} IS NOT A PALINDROME!`;
-            answerDiv.appendChild(isNotPalindrome);
+            span.innerHTML = word;
+            answerSentance.innerHTML = `IS NOT A PALINDROME!`;
         }
     }
+    input.value = '';
 }
 
 btn.addEventListener('click', checkIfPalindrome)
