bee9ab6fa31cdf53fcbf63649538ef0b74fdedb8
Alex Reid <alexatrs@hotmail.co.uk>
2021-01-29 16:13:10 +0000

    React project added

---
+++ adopt-me-project/src/app.js
@@ -0,0 +1,34 @@
+const Pet = ({ name, animal, breed}) => {
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, name),
+        React.createElement("h2", {}, animal),
+        React.createElement("h2", {}, breed)
+    ]);
+};
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {},
+        [
+            React.createElement("h1", {} , "Adopt Me!"),
+            React.createElement(Pet, {
+                name: "Chip",
+                animal: "Dog", 
+                breed: "Havanese"
+            }),
+            React.createElement(Pet, {
+                name: "Luna", 
+                animal: "Cat", 
+                breed: "Mixed"
+            }),
+            React.createElement(Pet, {
+                name: "Bob", 
+                animal: "Dog", 
+                breed: "Havanese"
+            })
+        ]
+    );
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
